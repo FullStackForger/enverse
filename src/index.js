7d8c10fbcd8b256c39149f57d13415d4cd83248e
FullStackForger <fullstackforger@gmail.com>
2016-10-26 21:14:08 +0100

    srs

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const enverse = {}
+
+Object.defineProperty(enverse, 'is', {
+    writable: false,
+    value: {
+        server: new Function("try {return this===global;}catch(e){return false;}"),
+        browser: new Function("try {return this===window;}catch(e){ return false;}")
+    }
+})
+
+Object.defineProperty(enverse, 'has', {
+    writable: false,
+    value: {
+        document: typeof document === 'object',
+        navigator: typeof document === 'navigator',
+        localStorage: typeof document === 'localStorage',
+        sessionStorage: typeof document === 'sessionStorage'
+    }
+})
+
+Object.defineProperty(enverse, 'has', {})
+export default enverse
