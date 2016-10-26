68de5948f133cb754db449f8832cd2bbf5cb183d
FullStackForger <fullstackforger@gmail.com>
2016-10-26 23:59:18 +0100

    test scripts

---
+++ src/index.spec.js
@@ -0,0 +1,12 @@
+import chai from 'chai'
+import jwtAuth from './index'
+
+const expect = chai.expect
+const assert = chai.assert
+
+describe('storage', () => {
+
+	describe('on the client', () => {
+	})
+
+})
