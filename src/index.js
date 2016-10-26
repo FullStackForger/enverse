ef7f1bcbaf7bba09a4490198a3b0713533584d48
FullStackForger <fullstackforger@gmail.com>
2016-10-27 00:31:09 +0100

    test coverage

--- src/index.js
+++ src/index.js
@@ -3,20 +3,21 @@ const enverse = {}
 Object.defineProperty(enverse, 'is', {
     writable: false,
     value: {
-        server: new Function("try {return this===global;}catch(e){return false;}"),
-        browser: new Function("try {return this===window;}catch(e){ return false;}")
+        server: (new Function("try {return this===global;}catch(e){return false;}"))(),
+        browser: (new Function("try {return this===window;}catch(e){ return false;}"))()
     }
 })
 
 Object.defineProperty(enverse, 'has', {
     writable: false,
     value: {
+				window: typeof window === 'object',
         document: typeof document === 'object',
-        navigator: typeof document === 'navigator',
-        localStorage: typeof document === 'localStorage',
-        sessionStorage: typeof document === 'sessionStorage',
-        global: typeof document === 'global',
-        process: typeof document === 'process'
+        navigator: typeof document === 'object',
+        localStorage: typeof document === 'object',
+        sessionStorage: typeof document === 'object',
+        global: typeof global === 'object',
+        process: typeof process === 'object'
     }
 })
 
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,12 +1,79 @@
-import chai from 'chai'
-import jwtAuth from './index'
-
-const expect = chai.expect
-const assert = chai.assert
+import {expect} from 'chai'
+import env from './index'
 
 describe('storage', () => {
 
-	describe('on the client', () => {
+	describe('in the browser', () => {
+
+		it('should detect detect', () => {
+			expect(env.is.server).to.be.false
+			expect(env.is.browser).to.be.true
+		})
+
+		it('should have window', () => {
+			expect(env.has.window).to.be.true
+		})
+
+		it('should have document', () => {
+			expect(env.has.document).to.be.true
+		})
+
+		it('should have navigator', () => {
+			expect(env.has.navigator).to.be.true
+		})
+
+		it('should have localStorage', () => {
+			expect(env.has.localStorage).to.be.true
+		})
+
+		it('should have sessionStorage', () => {
+			expect(env.has.sessionStorage).to.be.true
+		})
+
+		it('should not have global', () => {
+			expect(env.has.global).to.be.false
+		})
+
+		it('should not have process', () => {
+			expect(env.has.process).to.be.false
+		})
+	})
+
+	describe('on the server', () => {
+
+		it('should detect detect', () => {
+			expect(env.is.server).to.be.true
+			expect(env.is.browser).to.be.false
+		})
+
+		it('should not have window', () => {
+			expect(env.has.window).to.be.false
+		})
+
+		it('should not have document', () => {
+			expect(env.has.document).to.be.false
+		})
+
+		it('should not have navigator', () => {
+			expect(env.has.navigator).to.be.false
+		})
+
+
+		it('should not have localStorage', () => {
+			expect(env.has.localStorage).to.be.false
+		})
+
+		it('should not have sessionStorage', () => {
+			expect(env.has.sessionStorage).to.be.false
+		})
+
+		it('should have global', () => {
+			expect(env.has.global).to.be.true
+		})
+
+		it('should have process', () => {
+			expect(env.has.process).to.be.true
+		})
 	})
 
 })
