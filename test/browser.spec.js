51f68b1061ce0b99c2ac4334b5881449a05edf46
FullStackForger <fullstackforger@gmail.com>
2016-10-27 13:30:57 +0100

    better test coverage, #1

--- test/browser.spec.js
+++ test/browser.spec.js
@@ -1,12 +1,27 @@
 import {expect} from 'chai'
-import env from '../src/index'
 import jsdom from 'jsdom'
 
+const storage = {}
+
 describe('in the browser', () => {
+	var env
+
 	before(() => {
 		global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
 		global.window = document.defaultView
 		global.navigator = { userAgent: 'node.js' }
+		global.localStorage = global.localStorage = storage
+		window.localStorage = window.sessionStorage = storage
+		const storage = {
+        getItem: function (key) {
+            return this[key];
+        },
+        setItem: function (key, value) {
+            this[key] = value;
+        }
+    }
+		delete require.cache[require.resolve('../src/index')]
+		env = require('../src/index').default
 	})
 
 	it('should detect detect', () => {
@@ -35,10 +50,12 @@ describe('in the browser', () => {
 	})
 
 	it('should not have global', () => {
+		let process
 		expect(env.has.global).to.be.false
 	})
 
 	it('should not have process', () => {
+		let global
 		expect(env.has.process).to.be.false
 	})
 })
