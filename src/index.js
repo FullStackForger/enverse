1f2a3c519f9725a98e99fcfbd0edcbd9a4d201cb
FullStackForger <fullstackforger@gmail.com>
2016-10-27 15:05:43 +0100

    environment evaluation method

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
-const enverse = {}
+/**
+ * Evaluates environment available from the current scope and returns env object.
+ * Helper method created to facilitate testing.
+ */
+export const evaluate = function() {
+	const enverse = {}
 
-Object.defineProperty(enverse, 'is', {
-    writable: false,
-    value: {
-        server: (new Function("try {return this===global;}catch(e){return false;}"))(),
-        browser: (new Function("try {return this===window;}catch(e){ return false;}"))()
-    }
-})
+	Object.defineProperty(enverse, 'is', {
+			writable: false,
+			value: {
+					server: (() => { try { return this===this.global } catch(e) { return false }})(),
+					browser: (() => { try { return this===this.window } catch(e) { return false }})(),
+			}
+	})
 
-Object.defineProperty(enverse, 'has', {
-    writable: false,
-    value: {
-				window: typeof window === 'object',
-        document: typeof document === 'object',
-        navigator: typeof document === 'object',
-        localStorage: typeof document === 'object',
-        sessionStorage: typeof document === 'object',
-        global: typeof global === 'object',
-        process: typeof process === 'object'
-    }
-})
+	Object.defineProperty(enverse, 'has', {
+			writable: false,
+			value: {
+					window: typeof this.window === 'object',
+					document: typeof this.document === 'object',
+					navigator: typeof this.navigator === 'object',
+					localStorage: typeof this.localStorage === 'object',
+					sessionStorage: typeof this.sessionStorage === 'object',
+					global: typeof this.global === 'object',
+					process: typeof this.process === 'object'
+			}
+	})
 
-Object.defineProperty(enverse, 'has', {})
-export default enverse
+	return enverse
+}
+
+// Evaluate environment from global (server) or window (browser) level
+const env = evaluate.call((new Function('return this'))())
+export default env
